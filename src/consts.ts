a1d72aa6b81a4821fb2963d48341fd5f8a9edf6a
Nicolas TOISON <toisonnicolas@gmail.com>
2023-02-18 15:09:05 +0100

    feat(site): update site & description fields

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "My personal website.";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+export const SITE_TITLE = "Web Developer & Cloud administrator — Nicolas TOISON";
+export const SITE_DESCRIPTION = "I'm Nicolas TOISON, a web developer & cloud admin with expertise in building & managing web apps and cloud-based solutions. Explore my site to learn more.";
