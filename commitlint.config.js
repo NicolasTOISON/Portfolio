c29174bd59554db80eb983e6c9688b7df13883fc
Nicolas TOISON <toisonnicolas@gmail.com>
2024-07-25 13:57:40 +0200

    build(ci): override commitlint body-max-line-lenght rule for first exec

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'header-max-length': [0, 'always', 72],
+    'body-max-line-length': [0, 'always', 100],
   },
 }
